55bb79563a352ba377ee2788224cd33213a34ff0
yangzhie <arnavmalasi2212@gmail.com>
2026-09-07 14:41:39 +1000

    chore: bump SDK version to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.innovatex.auracast"
-        minSdk = 24
+        minSdk = 33
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
